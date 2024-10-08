65a0b047455d2efdf0e9739e4c76d728a1fd4637
Virender Prasad <virender288@gmail.com>
2024-10-08 16:48:07 +0530

    added the home section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import Navbar from "../Navbar";
+import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
+import Projects from "../Projects/Projects";
+import Skills from "../Skills/Skills";
+import Contact from "../Contact/Contact";
+import Footer from "../Footer/Footer";
 
 
 const Home = ({ toggleDarkMode, darkMode }) => {
@@ -8,6 +12,10 @@ const Home = ({ toggleDarkMode, darkMode }) => {
         <div className="bg-white dark:bg-zinc-900/90 text-black dark:text-white">
             <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode} /> 
             <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+            <Skills toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
+            <Projects toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+            <Contact toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+            <Footer toggleDarkMode={toggleDarkMode} darkMode={darkMode} />           
         </div>
     );
 };
