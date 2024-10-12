91893c249dcd235a965b8fd8692ed4807408e2f5
Virender Prasad <virender288@gmail.com>
2024-10-12 23:08:17 +0530

    added the skills sections

--- frontend/src/components/Skills/Skills.jsx
+++ frontend/src/components/Skills/Skills.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaGithub, FaGitAlt, FaFigma } from 'react-icons/fa';
-import { SiTypescript, SiPostgresql, SiMongodb, SiFlutter } from 'react-icons/si';
-import { RiTailwindCssFill } from "react-icons/ri";
+import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaGithub, FaGitAlt, FaNodeJs  } from 'react-icons/fa';
+import { SiTypescript, SiPostgresql, SiMongodb, SiExpress,SiPostman,SiAppwrite  } from 'react-icons/si';
+
 
 const Skills = () => {
   const skills = [
-    { Icon: FaHtml5, label: 'HTML5', color: 'text-orange-600', bgColor: 'bg-orange-200', darkBgColor: 'bg-orange-800' },
-    { Icon: FaCss3Alt, label: 'CSS3', color: 'text-blue-600', bgColor: 'bg-blue-200', darkBgColor: 'bg-blue-800' },
-    { Icon: FaJsSquare, label: 'JavaScript', color: 'text-yellow-600', bgColor: 'bg-yellow-200', darkBgColor: 'bg-yellow-800' },
-    { Icon: SiTypescript, label: 'TypeScript', color: 'text-blue-600', bgColor: 'bg-blue-200', darkBgColor: 'bg-blue-800' },
-    { Icon: FaReact, label: 'React.js', color: 'text-cyan-600', bgColor: 'bg-cyan-200', darkBgColor: 'bg-cyan-800' },
-    { Icon: FaGithub, label: 'GitHub', color: 'text-black', bgColor: 'bg-gray-200', darkBgColor: 'bg-gray-700' },
-    { Icon: FaGitAlt, label: 'Git', color: 'text-orange-600', bgColor: 'bg-orange-200', darkBgColor: 'bg-orange-800' },
-    { Icon: SiMongodb, label: 'MongoDB', color: 'text-green-600', bgColor: 'bg-green-200', darkBgColor: 'bg-green-800' },
-    { Icon: SiPostgresql, label: 'PostgreSQL', color: 'text-blue-600', bgColor: 'bg-blue-200', darkBgColor: 'bg-blue-800' },
+    { Icon: FaHtml5, label: 'HTML5', color: 'text-orange-600', bgColor: 'bg-orange-100', darkBgColor: 'bg-orange-700' },
+    { Icon: FaCss3Alt, label: 'CSS3', color: 'text-blue-600', bgColor: 'bg-blue-100', darkBgColor: 'bg-blue-700' },
+    { Icon: FaJsSquare, label: 'JavaScript', color: 'text-yellow-600', bgColor: 'bg-yellow-100', darkBgColor: 'bg-yellow-700' },
+    { Icon: FaNodeJs, label: 'NodeJs', color: 'text-lime-700', bgColor: 'bg-lime-200', darkBgColor: 'bg-lime-800' },
+    { Icon: SiExpress, label: 'ExpressJs', color: 'text-gray-600', bgColor: 'bg-gray-200', darkBgColor: 'bg-gray-800' },
+    { Icon: FaReact, label: 'React.js', color: 'text-cyan-600', bgColor: 'bg-cyan-100', darkBgColor: 'bg-cyan-700' },
+    { Icon: FaGithub, label: 'GitHub', color: 'text-black', bgColor: 'bg-gray-100', darkBgColor: 'bg-gray-800' },
+    { Icon: FaGitAlt, label: 'Git', color: 'text-orange-600', bgColor: 'bg-orange-100', darkBgColor: 'bg-orange-700' },
+    { Icon: SiTypescript, label: 'TypeScript', color: 'text-indigo-600', bgColor: 'bg-indigo-100', darkBgColor: 'bg-indigo-700' },
+    { Icon: SiMongodb, label: 'MongoDB', color: 'text-green-600', bgColor: 'bg-green-100', darkBgColor: 'bg-green-700' },
+    { Icon: SiPostgresql, label: 'PostgreSQL', color: 'text-blue-600', bgColor: 'bg-sky-100', darkBgColor: 'bg-sky-700' },
+    { Icon: SiPostman, label: 'Postman', color: 'text-orange-600', bgColor: 'bg-orange-100', darkBgColor: 'bg-orange-700' },
+    { Icon: SiAppwrite, label: 'Appwrite', color: 'text-pink-600', bgColor: 'bg-pink-100', darkBgColor: 'bg-pink-700' },
   ];
 
   return (
